9d58b5177fba626565f04340c8b8b24bc2364a89
Kyle Van Bergen <kyleavb@gmail.com>
2018-03-15 10:34:50 -0700

    test

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = (props) => (
         <li><a href='#'>Home</a></li>{' '}
         <li><a href='#'>Search</a></li>{' '}
         <li><a href='#'>Bookings</a></li>{' '}
-        <li><a href='#'>Login</a></li>{' '}
+        <li><a href='#'>Login</a></li>
       </ul>
     </nav>
   </div>
