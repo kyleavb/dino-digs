2267fed0d8947b1a9ff832866026bf7a35ffb5b4
Kyle Van Bergen <kyleavb@gmail.com>
2018-03-15 10:26:01 -0700

    created nav bar

---
+++ client/src/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = (props) => (
+  <div>
+    <nav>
+      <ul>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>Search</a></li>
+        <li><a href='#'>Bookings</a></li>
+        <li><a href='#'>Login</a></li>
+      </ul>
+    </nav>
+  </div>
+)
+
+export default NavBar
