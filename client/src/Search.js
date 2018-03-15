d908eda37246caf8c5c3b6ea174b266103b59242
Kyle Van Bergen <kyleavb@gmail.com>
2018-03-15 13:47:28 -0700

    pushing stuff

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import Caves from './cave_json'
 
 class Search extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      filterValue: '',
+      display: {Caves}
+    }
+  }
   render(){
     return(
       <div>
         <h1> Search </h1>
+        <label htmlFor="Locations">Locations: </label>
+        <input type='text' name='Locations' />
       </div>
     )
   }
--- client/src/UserProfile.js
+++ client/src/UserProfile.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-const UserProfile = (props) => (
-  <div className="user-profile">
-    <h2>User</h2>
-    <ul className="user-details">
-      <li>user email</li>{' '}
-      <li><a href='#'>Update Profile</a></li>{' '}
-      <li><a href='#'>Delete Profile</a></li>
-    </ul>
-  </div>
-)
+class UserProfile extends Component {
+  render(){
+    return(
+      <div>
+        <h1> UserProfile </h1>
+      </div>
+    )
+  }
+}
 
-export default UserProfile;
+export default UserProfile
